6bc91c0c06a56e2c03d023fd6dfe119287684d27
JihanDefina28 <125740824+JihanDefina28@users.noreply.github.com>
2023-05-06 19:44:32 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <script src="https://use.fontawesome.com/d1341f9b7a.js"></script>
+    <link rel="stylesheet" href="style.css" />
+    <title>JihanDefinaRani.com</title>
+  </head>
+  <body>
+        <div style="color:rgb(252, 252, 252); padding: 30px; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">
+            <center><h1 style="color:black;">Welcome, to My Personal Information</h1></center>
+    <div class="box">
+      <img src="foto-jihan.png" alt="" class="box-img" />
+      <h1>Jihan Defina Rani</h1>
+      <h5>Junior Cloud Engineer</h5>
+
+      <tr>
+        <td colspan=3>
+        <hr color= yellow></center>
+        <B>My Story</B>
+        <center><hr color=yellow> Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto veniam itaque autem quasi sunt.</center>
+        </td>
+      </tr>
+      <br>
+      <tr>
+        <td colspan=3>
+        <hr color= yellow></center>
+        <B>My Personal Information</B>
+        <hr color= yellow></center>
+        <center>Age: 21 years old.</center>
+        <center>Phone: 08212938.</center>
+        <center>Email: Jihand53@gmail.com.</center>
+        <center>Agency: SMTOWN.</center>
+        </td>
+      </tr>
+      <br>
+      <br>
+      <B>Find Me At</B>
+      <ul>
+        <li>
+          <a href="#"
+            ><i class="fa fa-github-square" aria-hidden="true"></i
+          ></a>
+        </li>
+        <li>
+          <a href="#"
+            ><i class="fa instagram-square" aria-hidden="true"></i
+          ></a>
+        </li>
+        <li>
+          <a href="#"
+            ><i class="fa fa-twitter-square" aria-hidden="true"></i
+          ></a>
+        </li>
+      </ul>
+    </div>
+  </body>
+</html>
