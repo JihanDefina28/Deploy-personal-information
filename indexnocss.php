e4fa02982c3fbd9970c62133f77bc1de37b0e270
JihanDefina28 <125740824+JihanDefina28@users.noreply.github.com>
2023-05-10 17:28:31 +0700

    indexnocss.php

---
+++ indexnocss.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>JihanDefinaRani.com</title>
+  </head>
+  <body>
+        <div style="color:rgb(252, 252, 252); padding: 30px; font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">
+            <center><h1 style="color:black;">Welcome, to My Personal Information</h1></center>
+        <td bgcolor="rgba(0, 0, 0, 0.4)">
+        <center><img
+            src="https://storage.googleapis.com/bucket-jihan-1/foto-jihan.png"
+            alt="Profile"
+            width="200"
+            height="200"
+            align="center"
+          /></center>
+          <br>
+          <hr>
+
+          <font face="sans-serif">
+          <center><h1 style="color:black;">Jihan Defina Rani</h1></center>
+          <center><h1 style="color:black;">Cloud Engineer</h1></center>
+          </font>
+
+      <tr>
+        <td colspan=3>
+        <center><hr color= yellow><center><h1 style="color:black;">My Story</h1></center>
+        <center><h3 style="color:black;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto veniam itaque autem quasi sunt.</h3></center>
+        </td>
+      </tr>
+      <br>
+      <tr>
+        <td colspan=3>
+        <center><hr color= yellow><center><h1 style="color:black;">My Personal Information</h1></center>
+        <center><h3 style="color:black;">Age: 21 years old.</h3></center>
+        <center><h3 style="color:black;">Phone: 08212938.</h3></center>
+        <center><h3 style="color:black;">Email: Jihand53@gmail.com.</h3></center>
+        <center><h3 style="color:black;">Agency: SMTOWN.</h3></center>
+        </td>
+      </tr>
+      <br>
+      <br>
+      <center><h3 style="color:black;">Find Me At</h3></center>
+      <ul>
+        <li>
+          <center><href="#"><img src="https://storage.googleapis.com/bucket-jihan-1/github.png" height="40px" width="40px" alt="GitHub"></a>&nbsp; &nbsp;
+          ></a></center>
+        </li>
+        <br>        
+        <li>
+        <center><a href="#"><img src="https://storage.googleapis.com/bucket-jihan-1/twitter.png" height="40px" width="40px" alt="Twitter"></a>&nbsp; &nbsp;
+          ></a></center>
+        </li>
+      </ul>
+    </div>
+  </body>
+</html>
